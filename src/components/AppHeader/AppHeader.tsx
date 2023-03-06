ffaf0a1d48756404a8737dec16eac339625c8825
Sergei Zakharchenko <millennial404@yandex.ru>
2023-03-07 00:04:53 +0300

    feat: create Header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -3,17 +3,33 @@ import {
   BurgerIcon,
   ListIcon,
   ProfileIcon,
+  Button,
 } from "@ya.praktikum/react-developer-burger-ui-components";
-import styles from "./AppHeader.module.css"
+import styles from "./AppHeader.module.css";
 
 export default function AppHeader() {
   return (
-    <header className="header">
+    <header className={styles.header}>
       <BurgerIcon type="primary" />
-      <ListIcon type="primary" />
-      <Logo />
+      <div
+        style={{ backgroundColor: "#0B5FFF", color: "white" }}
+        className="m-2"
+      >
+      </div>
+      <p className="text text_type_main-default">Конструктор</p>
 
-      <ProfileIcon type="primary" />
+      <button className={styles.button}>
+        <ListIcon type="primary" />
+        Лента заказов
+      </button>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+
+      <button className={styles.button}>
+        <ProfileIcon type="primary" />
+        Личный кабинет
+      </button>
     </header>
   );
 }
