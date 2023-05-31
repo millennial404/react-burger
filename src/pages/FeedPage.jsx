b1a62a741858058cf6e4041815af2eda30f7e047
Sergei Zakharchenko <millennial404@yandex.ru>
2023-06-01 00:11:30 +0300

    feat: FeedPage/OrdersBoard

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -23,16 +23,71 @@ const OrderCard = () => {
                                                src="https://code.s3.yandex.net/react/code/meat-03.png" alt=""/></li>
           <li className={style.component}><img className={style.componentsImg}
                                                src="https://code.s3.yandex.net/react/code/bun-02.png" alt=""/></li>
-
         </ul>
         <div className={style.price}><span className="text text_type_digits-default mr-2">460 </span> <CurrencyIcon
           type="primary"/></div>
-
       </div>
     </li>
 
   )
 }
+
+const OrdersBoard = () => {
+  return (
+    <div className={`${style.orderBoardContainer} ml-15`}>
+      <div className={`${style.ordersStatus} mb-15`}>
+        <div className={`${style.ordersReadyContainer}`}>
+          <h3 className="text text_type_main-medium mb-6">Готовы:</h3>
+          <ul className={`${style.ordersReadyElements} mr-9`}>
+            <li className={`${style.ordersReadyElement}`}><p className={`${style.readyOrderNumber} text text_type_digits-default mb-2`}>034535</p></li>
+            <li className={`${style.ordersReadyElement}`}><p className={`${style.readyOrderNumber} text text_type_digits-default mb-2`}>034535</p></li>
+            <li className={`${style.ordersReadyElement}`}><p className={`${style.readyOrderNumber} text text_type_digits-default mb-2`}>034535</p></li>
+            <li className={`${style.ordersReadyElement}`}><p className={`${style.readyOrderNumber} text text_type_digits-default mb-2`}>034535</p></li>
+            <li className={`${style.ordersReadyElement}`}><p className={`${style.readyOrderNumber} text text_type_digits-default mb-2`}>034535</p></li>
+            <li className={`${style.ordersReadyElement}`}><p className={`${style.readyOrderNumber} text text_type_digits-default mb-2`}>034535</p></li>
+          </ul>
+        </div>
+        <div className={`${style.ordersInWorkContainer}`}>
+          <h3 className="text text_type_main-medium mb-6">В работе:</h3>
+          <ul className={`${style.ordersInWorkElements}`}>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+            <li className={`${style.ordersInWorkElement}`}>
+              <p className="text text_type_digits-default mb-2">034535</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <h3 className="text text_type_main-medium">Выполнено за все время:</h3>
+      <p className={`${style.shadow} text text_type_digits-large mb-15`}>28 752</p>
+      <h3 className="text text_type_main-medium">Выполнено за сегодня:</h3>
+      <p className={`${style.shadow} text text_type_digits-large mb-2`}>138</p>
+    </div>
+  )
+}
+
 export const FeedPage = () => {
   return (
     <main className={style.main}>
@@ -48,6 +103,7 @@ export const FeedPage = () => {
         <OrderCard/>
         <OrderCard/>
       </ul>
+      <OrdersBoard/>
     </main>
 
   )
